5c2141264f2648dbb16b879550f3f857e9c0afef
KasiClk <clark.kasongo18@gmail.com>
2022-05-02 17:58:27 +0200

    Init popolaDB

---
+++ src/main/java/it/accademyfullstack/jpadata/town/util/PopolaDB.java
@@ -0,0 +1,12 @@
+package it.accademyfullstack.jpadata.town.util;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class PopolaDB implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
